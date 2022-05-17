f1e79399b07425dae545afab1bf57ce0724f61bd
Thach Nguyen <thachnn80@gmail.com>
2022-05-17 17:27:34 +0700

    Build `libunwind` from LLVM source code

--- __build_libunwind.sh
+++ __build_libunwind.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
-_SC_DIR="$(cd "`dirname "$0"`"; pwd)"
 
 _PKG=libunwind-9.0.0.src
 _PREFIX="$1"
@@ -8,17 +7,28 @@ _SCRATCH_DIR="$2"
 
 if [[ ! -e "$_PREFIX/lib/libunwind.a" ]]
 then
+  _VER="$( "`dirname "$0"`/get_clang_ver.sh" || echo "${_PKG:10:5}" )"
+  _PKG="${_PKG%-*}-$_VER.${_PKG##*.}"
+  _DEP="llvm-${_PKG#*-}"
+
   cd "$_SCRATCH_DIR"
-  [[ -s "$_PKG.tar.xz" ]] || \
-    curl -OkSL "https://releases.llvm.org/${_PKG:10:5}/$_PKG.tar.xz"
+  [[ -s "$_PKG.tar.xz" ]] || curl -OkfSL "https://releases.llvm.org/$_VER/$_PKG.tar.xz"
+  [[ -s "$_DEP.tar.xz" ]] || curl -OkfSL "https://releases.llvm.org/$_VER/$_DEP.tar.xz"
 
-  # Install headers
-  tar -C "$_PREFIX" -xf "$_PKG.tar.xz" --strip-components=1 "$_PKG/include"
+  rm -rf "$_PKG"
+  tar -xf "$_PKG.tar.xz"
+  tar -C "$_PKG" -xf "$_DEP.tar.xz" --strip-components=1 "$_DEP/cmake/modules"
 
-  # Install static library
-  cd "$_PREFIX/lib"
-  git apply "$_SC_DIR/libunwind.patch"
+  cd "$_PKG"
+  sed -i- 's/^if (EXISTS \${LLVM_CMAKE_PATH}/& AND LLVM_INCLUDE_TESTS/' CMakeLists.txt
 
-  echo '2b89581fe31d091a1bbe015beda13565598f148785c85c0cf9e317a3a68f5b48 *libunwind.a' \
-    | shasum -c
+  cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_FRAMEWORK=LAST \
+    -DCMAKE_VERBOSE_MAKEFILE=ON -Wno-dev "-DCMAKE_INSTALL_PREFIX=$_PREFIX" \
+    "-DLLVM_PATH=$PWD" -DLLVM_INCLUDE_TESTS=OFF -DLIBUNWIND_ENABLE_SHARED=OFF
+
+  # Use relative paths
+  find */CMakeFiles -name build.make -exec sed -i- "s:-c $PWD/[^ /]*/:-c :" {} +
+
+  make -j2 install
+  cp -af include "$_PREFIX"
 fi
