4dac95e12ed4201b4ead71bd368110a7e2a81430
Thach Nguyen <thachnn80@gmail.com>
2022-06-11 22:05:54 +0700

    Implement unit-tests for `libunwind` builder(s)

--- __build_libunwind.sh
+++ __build_libunwind.sh
@@ -1,39 +1,49 @@
 #!/bin/bash
 set -xe
-_SC_DIR="$(dirname "$0")"
+_SC_DIR="$(cd "`dirname "$0"`"; pwd)"
 
 _PKG=libunwind-9.0.0.src
 _PREFIX="$1"
 _SCRATCH_DIR="$2"
+_NO_TESTS="$3"
 
 if [[ ! -e "$_PREFIX/lib/libunwind.a" ]]
 then
   _VER="$( "$_SC_DIR/get_clang_ver.sh" || echo "${_PKG:10:5}" )"
   _PKG="${_PKG%-*}-$_VER.${_PKG##*.}"
+  _DEP="llvm-${_PKG#*-}"
 
   cd "$_SCRATCH_DIR"
-  [[ -s "$_PKG.tar.xz" ]] || \
-    curl -OkfSL "https://github.com/llvm/llvm-project/releases/download/llvmorg-$_VER/$_PKG.tar.xz" || \
-    curl -OkfSL "https://releases.llvm.org/$_VER/$_PKG.tar.xz"
+  [[ -s "$_PKG.tar.xz" ]] || "$_SC_DIR/download_llvm_pkg.sh" "$_PKG" "$_VER"
+  [[ -s "$_DEP.tar.xz" ]] || "$_SC_DIR/download_llvm_pkg.sh" "$_DEP" "$_VER"
+
   rm -rf "$_PKG"
   tar -xf "$_PKG.tar.xz"
+  tar -C "$_PKG" -xf "$_DEP.tar.xz" --strip-components=1 "$_DEP/cmake/modules" "$_DEP/utils/lit"
 
-  # Download dependencies
-  cd "$_PKG/cmake/modules"
-  curl -kfSL "https://api.github.com/repos/llvm/llvm-project/contents/llvm/cmake/modules?ref=llvmorg-$_VER" \
-    | grep '"download_url":' | sed -e 's/,*$//;s/^.*"download_url": */-OskfSL /' | xargs curl
-
-  cd ../..
+  cd "$_PKG"
   # Disable tests
-  sed -i- 's/^if (EXISTS \${LLVM_CMAKE_PATH}/& AND LLVM_INCLUDE_TESTS/' CMakeLists.txt
+  sed -i- $'s/^ *add_subdirectory(test)/if (LLVM_INCLUDE_TESTS)\\\n&\\\nendif()/' CMakeLists.txt
 
   cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_FRAMEWORK=LAST \
     -DCMAKE_VERBOSE_MAKEFILE=ON -Wno-dev "-DCMAKE_INSTALL_PREFIX=$_PREFIX" \
-    "-DLLVM_PATH=$PWD" -DLLVM_INCLUDE_TESTS=OFF -DLIBUNWIND_ENABLE_SHARED=OFF
+    "-DLLVM_PATH=$PWD" "-DLLVM_EXTERNAL_LIT=$PWD/utils/lit/lit.py" "-DLIBUNWIND_LIBCXX_PATH=$PWD" \
+    -DLIBUNWIND_ENABLE_SHARED=OFF -DLLVM_INCLUDE_TESTS=$([[ "$_NO_TESTS" == 0 ]] && echo ON || echo OFF)
 
   # Use relative paths
   find */CMakeFiles -name build.make -exec sed -i- "s:-c $PWD/[^ /]*/:-c :" {} +
 
   make -j2 install
   cp -af include "$_PREFIX"
+
+  if [[ "$_NO_TESTS" == 0 ]]; then
+    cd ..
+    _DEP="libcxx-${_PKG#*-}"
+    [[ -s "$_DEP.tar.xz" ]] || "$_SC_DIR/download_llvm_pkg.sh" "$_DEP" "$_VER"
+    tar -C "$_PKG" -xf "$_DEP.tar.xz" --strip-components=1 "$_DEP/utils/libcxx" \
+      "$_DEP/test"/{std/input.output/filesystems/Inputs,support/filesystem_dynamic_test_helper.py}
+
+    cd "$_PKG"
+    make check-unwind
+  fi
 fi
